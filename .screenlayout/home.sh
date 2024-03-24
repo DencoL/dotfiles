9d56279d44f4d1f4a2c9b9a9f0b4102fa34b9fdf
Denis Lochman <denis.lochman@protonmail.com>
2024-03-24 22:22:30 +0100

    added arandr setup script

---
+++ .screenlayout/home.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output HDMI-1 --mode 1920x1080 --pos 2560x0 --rotate right --output HDMI-2 --off --output DP-1 --primary --mode 2560x1440 --pos 0x0 --rotate normal --output HDMI-3 --off
